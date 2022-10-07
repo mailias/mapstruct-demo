5048b73187fea5e883712e5e3d77fa5e8aa21a4f
Fischer Matthias <Matthias.Fischer@doubleSlash.de>
2022-10-07 13:56:19 +0200

    add CommandLineRunner in order to run code on startup (for possible later usage)

---
+++ src/main/java/com/example/demo/Runner.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        // here maybe later some db data init
+
+
+    }
+}
